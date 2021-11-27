3f6139ab3f2ca698a02948b165206287f8b831c0
Omar R. Gelves Barahona <Omar>
2021-11-26 23:20:06 -0500

    Add layer graphql with resolver and shema

--- index.js
+++ index.js
@@ -1,56 +1,10 @@
-const ProjectModel = require('./model/proyectoModel')
+
+require('./infraestructura/conectionDB')
+const typeDefs = require('./typeDef')
+const resolvers = require('./resolver')
+
 const express = require('express')
-const { gql,ApolloServer } = require('apollo-server-express')
-
-const typeDefs = gql`
-
-type Usuario{
-    nombre: String
-    identificacion: Int
-    estado: String
-    email: String
-    perfil: String
-}
-type Query{
-    usuarios: [Usuario]
-    usuario(identificacion: Int): Usuario
-}
-`
-const listUsuarios = [
-    {
-        nombre: 'Ramon Castaño',
-        identificacion: 123456789,
-        estado: 'activo',
-        email: 'ramon@gmail.com',
-        perfil: 'estudiante'
-    },
-    {
-        nombre: 'Ernesto',
-        identificacion: 98765,
-        estado: 'activo',
-        email: 'ernesto@gmail.com',
-        perfil: 'estudiante'
-    },
-    {
-        nombre: 'Daniel Saavedra',
-        identificacion: 12345,
-        estado: 'activo',
-        email: 'daniel@gmail.com',
-        perfil: 'lider'
-    },
-]
-
-
-const resolvers = {
-    Query: {
-        usuarios: () => listUsuarios,
-        usuario: ( parent,args,context,info) => {
-           return listUsuarios.find(user=> user.identificacion===args.identificacion)
-        }
-    }
-}
-const api = express();
-
+const { ApolloServer } = require('apollo-server-express')
 
 const iniciarServidor =async () => {
     const api = express();
---
+++ resolver.js
@@ -0,0 +1,34 @@
+const Project = require('./model/proyectoModel')
+const listUsuarios = [
+    {
+        nombre: 'Ramon Castaño',
+        identificacion: 123456789,
+        estado: 'activo',
+        email: 'ramon@gmail.com',
+        perfil: 'estudiante'
+    },
+    {
+        nombre: 'Ernesto',
+        identificacion: 98765,
+        estado: 'activo',
+        email: 'ernesto@gmail.com',
+        perfil: 'estudiante'
+    },
+    {
+        nombre: 'Daniel Saavedra',
+        identificacion: 12345,
+        estado: 'activo',
+        email: 'daniel@gmail.com',
+        perfil: 'lider'
+    },
+]
+
+
+const resolvers = {
+    Query: {
+        usuarios: () => listUsuarios,
+        usuario: (parent, args, context, info) => listUsuarios.find(user => user.identificacion === args.identificacion),
+        proyectos: async () => await Project.find({})
+    }
+}
+module.exports = resolvers
